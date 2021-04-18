9fc25b4c9db38b2fc2f4af0f8e17b1867f7f48dc
Администратор <@localhost>
2021-04-18 16:58:08 +0300

    Создание хранилища конфигурации

--- git_init.cmd
+++ git_init.cmd
@@ -1,3 +1,3 @@
 @chcp 65001
 set GITSYNC_V8VERSION=8.3.18
-gitsync init --storage-user ci-bot --storage-pwd ci-bot C:/development/repo/demo C:/git/unit_tests/src
+gitsync init --storage-user ci-bot --storage-pwd ci-bot C:/development/repo/demo C:/git/First-steps-vanessa-runner/src
--- sync_repo.cmd
+++ sync_repo.cmd
@@ -1,3 +1,2 @@
 @chcp 65001
-set GITSYNC_V8VERSION=8.3.18
-gitsync --v8version 8.3.18 sync --storage-user ci-bot --storage-pwd ci-bot --limit 3 C:/development/repo/demo C:/git/unit_tests/src
+gitsync --v8version 8.3.18 sync --storage-user ci-bot --storage-pwd ci-bot --limit 3 C:/development/repo/demo C:/git/First-steps-vanessa-runner/src
