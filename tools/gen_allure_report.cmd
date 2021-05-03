dddf3861a2aeb0d2a978aa0588d9414c0eeb5cdd
Администратор <@localhost>
2021-05-03 23:11:25 +0300

    исправлена ошибка генерации отчета, если нет файла

--- tools/gen_allure_report.cmd
+++ tools/gen_allure_report.cmd
@@ -1,6 +1,8 @@
 echo "Copy last history"
+if NOT exist .\out\html_report\history\ GOTO :GEN 
 robocopy .\out\html_report\history\ .\out\syntaxCheck\allure\history
 
+:GEN 
 @rem Конвертация данных allure и вывод отчета
 @call allure generate --clean .\out\syntaxCheck\allure .\out\smoke  -o .\out\html_report
 @call allure open .\out\html_report
