f647f4f61279e6f259a14f750fcc5bb900a29c25
Администратор <@localhost>
2021-05-16 18:17:57 +0300

    изменены настройки запуска дымовых и юнит-тестов

--- tools/gen_allure_report.cmd
+++ tools/gen_allure_report.cmd
@@ -4,5 +4,6 @@ robocopy .\out\html_report\history\ .\out\syntaxCheck\allure\history
 
 :GEN 
 @rem Конвертация данных allure и вывод отчета
-@call allure generate --clean .\out\syntaxCheck\allure .\out\smoke  -o .\out\html_report
+@call allure generate --clean .\out\syntaxCheck\allure .\out\smoke -o .\out\html_report
+rem @call allure generate --clean .\out\syntaxCheck\allure -o .\out\html_report
 @call allure open .\out\html_report
